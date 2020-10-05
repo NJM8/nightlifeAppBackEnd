4bd83d316f2bf67cb6e75cf3bcbe4e9a904e70d5
natemallison <healthynate@gmail.com>
2020-10-05 09:45:46 -0400

    Mongoose updates.

--- models/index.js
+++ models/index.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.set('debug', true)
-mongoose.Promise = global.Promise
+mongoose.set("debug", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.Promise = global.Promise;
 
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/nightlifeapp')
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/nightlifeapp", {
+    useNewUrlParser: true,
+  })
   .then(() => {
-    return console.log('Connected to Mongo DB')
+    return console.log("Connected to Mongo DB");
   })
-  .catch(err => {
-    console.log(`Error: ${err}`)
-  })
+  .catch((err) => {
+    console.log(`Error: ${err}`);
+  });
 
-module.exports.Users = require('./UsersModel')
-module.exports.Bars = require('./BarsModel')
+module.exports.Users = require("./UsersModel");
+module.exports.Bars = require("./BarsModel");
